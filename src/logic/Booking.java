b18ae2a3fdc24cce5ecf1fc32c38ddca8c3df999
Kyueks-QX <teodorangelovldx@gmail.com>
2025-03-28 23:53:06 +0200

    TimeHandler works. findDate() moved to TimeHandler. Booking now checks for date conflicts.

--- src/logic/Booking.java
+++ src/logic/Booking.java
@@ -11,7 +11,6 @@ import java.util.ArrayList;
 //Has a lot of structural issues.
 public class Booking {
     private Calendar calendar;
-    private TimeHandler timeHandler;
 
     public Calendar getCalendar() {
         return calendar;
@@ -21,14 +20,7 @@ public class Booking {
         this.calendar = calendar;
     }
 
-    public TimeHandler getTimeHandler() {
-        return timeHandler;
-    }
-
-    public void setTimeHandler(TimeHandler timeHandler) {
-        this.timeHandler = timeHandler;
-    }
-
+    //it finds if a date exists
     private Date findDate(LocalDate day, LocalTime startTime, LocalTime endTime) {
         if (calendar.getDates().isEmpty()) { return null; }
 
@@ -44,23 +36,28 @@ public class Booking {
         return null;
     }
 
-    //not made to check for conflicting dates yet
+    //adds an entry to the calendar if there are no conflicting (overlapping) dates
     public boolean book(LocalDate day, LocalTime startTime, LocalTime endTime, String name, String note) {
-        ArrayList<Date> dates = calendar.getDates();
-        dates.add(new Date.Builder()
+        Date newDate = new Date.Builder()
                 .withDay(day)
                 .withStartTime(startTime)
                 .withEndTime(endTime)
                 .withName(name)
                 .withNote(note)
-                .build()
-        );
-        calendar.setDates(dates);
-        return true;
+                .build();
+
+        if (TimeHandler.isThereDateConflict(calendar, newDate)) {
+            ArrayList<Date> dates = calendar.getDates();
+            dates.add(newDate);
+            calendar.setDates(dates);
+            return true;
+        }
+        return false;
     }
 
+    //it just unbooks by removing an entry from the list
     public boolean unbook(LocalDate day, LocalTime startTime, LocalTime endTime) {
         if (calendar.getDates().isEmpty()) { return false; }
-        return calendar.getDates().remove(findDate(day, startTime, endTime));
+        return calendar.getDates().remove(TimeHandler.findDate(calendar, day, startTime, endTime));
     }
 }
--- src/logic/TimeHandler.java
+++ src/logic/TimeHandler.java
@@ -1,6 +1,33 @@
 package logic;
 
-//Makes sure the dates don't conflict.
-//Not in use yet.
+import models.Calendar;
+import models.Date;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class TimeHandler {
+    public static Date findDate(Calendar calendar, LocalDate day, LocalTime startTime, LocalTime endTime) {
+        if (calendar.getDates().isEmpty()) { return null; }
+
+        Date searchDate = new Date.Builder()
+                .withDay(day)
+                .withStartTime(startTime)
+                .withEndTime(endTime)
+                .build();
+
+        for (Date d : calendar.getDates()) {
+            if (d.equalsDayStartEndTime(searchDate)) { return d; }
+        }
+        return null;
+    }
+
+    public static boolean isThereDateConflict(Calendar calendar, Date newDate) {
+        for (Date date : calendar.getDates()) {
+            if (date.getEndTime().isAfter(newDate.getStartTime()) && date.getDay().isEqual(newDate.getDay())) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
